89f3326d302ed71d5ea54c07d92815b51c545814
Medhamsh V <medhamsh@axelerant.com>
2015-05-04 15:16:24 +0530

    adding settings

--- docroot/mysite/mysite/settings.py
+++ docroot/mysite/mysite/settings.py
@@ -58,8 +58,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'NAME': 'anotherdev',
+	'USER': 'anotherdev',
+	'PASSWORD': 'Bikhokdyx8',
+	'HOST': '127.0.0.1',
+	'PORT': '3306',
     }
 }
 
