655e15921e4d172e6e4c6a3e076749b91290b745
Medhamsh V <medhamsh@axelerant.com>
2015-05-04 11:00:00 +0530

    adding docroot with django

--- docroot/django/tests/admin_custom_urls/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-
-from .models import site
-
-urlpatterns = [
-    url(r'^admin/', include(site.urls)),
-]
--- docroot/django/tests/proxy_models/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-
-from .admin import site
-
-urlpatterns = [
-    url(r'^admin/', include(site.urls)),
-]
